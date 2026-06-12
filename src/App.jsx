4d2441dc9343cce3ec108d7ee73837295ef4a681
Shivangi <sp3671987@gmail.com>
2026-06-12 06:52:47 +0530

    01-folder

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+function App(){
+  return ( 
+       <>
+        <h1>React Folder Structure</h1>
+        <p>In this video, we will learn about the folder structure of a React application. We will also learn about the different files and folders that are present in a React application.</p>
+       </>
+  )
+}
+
+export default App
